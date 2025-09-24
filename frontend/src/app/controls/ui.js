3dd73d8ce370b4243f4fc1b30d07e213f0c07245
ikegodDF <takahashi.ryosuke.t6@elms.hokudai.ac.jp>
2025-09-24 16:18:49 +0900

    ボタン(UI)の仮作成

--- frontend/src/app/controls/ui.js
+++ frontend/src/app/controls/ui.js
@@ -1,5 +1,18 @@
+import { flyToJapan } from '../utils/camera.js';
+import '../../styles/ui.css';
+
 export function initUI(viewer) {
-  // ここでボタン等を動的に追加する
+  if (document.getElementById('uiControls')) return;
+
+  const container = document.createElement('div');
+  container.id = 'uiControls';
+
+  const btnFlyJapan = document.createElement('button');
+  btnFlyJapan.textContent = '初期位置へ';
+  btnFlyJapan.addEventListener('click', () => flyToJapan(viewer));
+
+  container.appendChild(btnFlyJapan);
+  document.body.appendChild(container);
 }
 
 
--- frontend/src/app/utils/camera.js
+++ frontend/src/app/utils/camera.js
@@ -2,8 +2,8 @@ import { Cartesian3, Math as CesiumMath } from 'cesium';
 
 export function flyToJapan(viewer) {
   viewer.camera.flyTo({
-    destination: Cartesian3.fromDegrees(138, 29, 4000000),
-    orientation: { heading: 0, pitch: -1.4, roll: 0 },
+    destination: Cartesian3.fromDegrees(141.925, 42.563, 500),
+    orientation: { heading: 0, pitch: -20 * CesiumMath.PI / 180, roll: 0 },
     duration: 2
   });
 }
