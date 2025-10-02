095deeadd0a4fafe0b0186980bcacfb68b063608
ikegodDF <takahashi.ryosuke.t6@elms.hokudai.ac.jp>
2025-10-02 13:59:13 +0900

    app内管理ステータスの実装

--- frontend/src/app/state/appState.js
+++ frontend/src/app/state/appState.js
@@ -1,5 +1,24 @@
 export const appState = {
+  year: new Date().getFullYear(),
+  appliedPolicy:[],
+  disasterState:[],
   selectedLayer: null,
 };
+
+export const setYear = (year) => {
+  appState.year = year;
+}
+
+export const setAppliedPolicy = (appliedPolicy) => {
+  appState.appliedPolicy = appliedPolicy;
+}
+
+export const setDisasterState = (disasterState) => {
+  appState.disasterState = disasterState;
+}
+
+export const setSelectedLayer = (selectedLayer) => {
+  appState.selectedLayer = selectedLayer;
+}
 
 
