82e5816031466d2b1f10b77c6d1d0e1ca629f252
ikegodDF <takahashi.ryosuke.t6@elms.hokudai.ac.jp>
2026-04-16 19:38:45 +0900

    画面上部に被害建物数を表示するように修正

--- frontend/src/app/controls/handlers/resultHandlers.js
+++ frontend/src/app/controls/handlers/resultHandlers.js
@@ -2,10 +2,21 @@ import { appState } from '../../state/appState.js';
 
 export const result = (viewer, models, outputContainer) => {
   const resultYear = appState.year;
-  const modelNames = new Set((models || []).map(m => m && m.name));
-  const visibleEntityCount = viewer.entities.values.filter(entity => modelNames.has(entity.name) && entity.show === true).length;
+  const safeModels = Array.isArray(models) ? models : [];
+  const modelNames = new Set(safeModels.map(m => m && m.name));
+  let visibleEntityCount = 0;
+  for (const entity of viewer.entities.values) {
+    if (modelNames.has(entity.name) && entity.show === true) visibleEntityCount += 1;
+  }
+  const damagedBuildingCount = safeModels.filter(model => model?.show === false).length;
+  const damageCountLabel =
+    appState.disasterState === '地震発生後'
+      ? '地震被害建物数'
+      : appState.disasterState === '津波発生後'
+        ? '津波被害建物数'
+        : '被害建物数';
   const disasterStateDisplay = appState.disasterState !== '被災前' 
-    ? `<br>被害状況:${appState.disasterState}` 
+    ? `<br>被害状況:${appState.disasterState}　　${damageCountLabel}:${damagedBuildingCount}` 
     : '';
   outputContainer.innerHTML = `施策:${appState.appliedPolicy}　　年度:${resultYear} 　　建物数:${visibleEntityCount}${disasterStateDisplay}`;
 };
